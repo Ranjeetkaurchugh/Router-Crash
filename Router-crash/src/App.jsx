73643bede70b4214f78044853486a6bf966a4cc8
Ranjeet kaur chugh <102470805+Ranjeetkaurchugh@users.noreply.github.com>
2023-10-17 15:11:24 +0000

    1st done

---
+++ Router-crash/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-green-400 text-center px-10 py-10'>Router-Crash Course</h1>
+  )
+}
+
+export default App
